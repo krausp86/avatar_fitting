21b5d52ab9a18521e6791755c94bcd8c35f24d4d
Patrick Kraus-Füreder <patrick.kraus-fuereder@kmlvision.com>
2026-03-20 21:34:25 +0100

    GPU bug

--- avatar_system/settings.py
+++ avatar_system/settings.py
@@ -53,7 +53,7 @@ WSGI_APPLICATION = 'avatar_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DATABASE_URL', str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
